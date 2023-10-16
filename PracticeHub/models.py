0217a7bebca0baf08cfc29e0ec372f9ed4ad4cdf
Ibtesam Kadourah <ibtesam.kadourah@gmail.com>
2023-10-16 11:04:10 +0300

    Add Basic models and create migrations

--- PracticeHub/models.py
+++ PracticeHub/models.py
@@ -2,14 +2,20 @@ from django.db import models
 
 # Create your models here.
 class Article (models.Model):
-    name = models.CharField(max_length=10)
+    name = models.CharField(max_length=10,unique=True)
     color = models.CharField(max_length=15)
+
+    def __str__(self) -> str:
+        return  self.name+"("+self.color+")"
 
 
 
 class Noun (models.Model):
-    name = models.CharField()
-    plural = models.CharField()
-    plural_abbr = models.CharField()
+    name = models.CharField(max_length=50,unique=True)
+    plural = models.CharField(max_length=75)
+    plural_abbr = models.CharField(max_length=50)
     singular_article = models.ForeignKey(Article, on_delete=models.PROTECT) #SET_NULL
+
+    def __str__(self) -> str:
+        return self.singular_article.name+" "+self.name+" ,"+self.plural_abbr+"=> die "+self.plural
 
