8773fdce8a156ffa296c0684ebc3d449ac87b9d4
ibtesam <ibtesam.kadourah@gmail.com>
2024-06-05 10:30:27 +0300

    add routes and templates for views

--- PracticeHub/urls.py
+++ PracticeHub/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
-    path('guess',views.guess_article),
+    path('quiz', views.quiz, name="quiz"),
+    path('nouns',views.nouns, name = "nouns"),
 ]
