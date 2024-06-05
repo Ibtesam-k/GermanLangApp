cdba932b943253ab48734a5fa7b1cb93f1a4c24e
ibtesam <ibtesam.kadourah@gmail.com>
2024-06-05 10:29:05 +0300

    create views add 2 views one for the quiz page another for nouns table

--- PracticeHub/views.py
+++ PracticeHub/views.py
@@ -1,30 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 import random
-from .models import Noun
+from .models import Noun, Article
 # Create your views here.
 
 def home(request):
-    return HttpResponse("welcome")
+      return render(request,"PracticeHub/home.html")
+
+def quiz(request):
+    print("Hallo")
+    ids = Noun.objects.values_list('id', flat=True)
+    randomId= random.choice(ids)
+    randomNoun  = Noun.objects.select_related("singular_article").get(pk = randomId)
+    articles = Article.objects.all()
+    print(articles)
+    context = {"title":"Noun Quiz","noun":randomNoun,"articles":articles}
+    return render (request, "PracticeHub/quiz.html", context)
 
 
+def nouns(request):
+    allNouns  = Noun.objects.select_related("singular_article").all()
+    return render(request,"PracticeHub/nouns.html",{'nouns':allNouns})
 
-def guess_article (request):
-    msg = ""
-    if request.method == 'POST':
-        print('hi')
-        answer = request.POST.get('article')
-        nounId = request.POST.get('noun')
-        randomNoun = Noun.objects.select_related("singular_article").get(pk = nounId)
-        if randomNoun.singular_article.name == answer :
-            msg = "correct"
-        else:
-            msg = "false"
-        print(answer)
-    else :
-        
-        ids = Noun.objects.values_list('id', flat=True)
-        randomId= random.choice(ids)
-        randomNoun  = Noun.objects.get(pk = randomId)
-        print(randomNoun)
-    return render(request,"guess.html",{'noun':randomNoun,'msg':msg})
